2f4bfbf3bbe7f566c53eaec346eab2344f1adb4c
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-01-16 23:34:08 +0900

    change string -> []byte

--- program/types.go
+++ program/types.go
@@ -3,16 +3,16 @@ package program
 // this type is the Thue program.
 type Program struct {
 	Rules []*Rule
-	Data  string
+	Data  []byte
 }
 
 // this type is the rule of the Thue.
 type Rule struct {
-	Name   string
+	Name   []byte
 	Action Rewriter
 }
 
 // this type is an rewrite action of the Thue.
 type Rewriter interface {
-	Rewrite(left, right string) (string, error)
+	Rewrite(left, right []byte) ([]byte, error)
 }
