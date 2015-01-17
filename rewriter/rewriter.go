8abcb1fba0cbc1d761aea1755cf5a3ec7a481005
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-01-17 11:34:39 +0900

    fix reader bug

--- rewriter/rewriter.go
+++ rewriter/rewriter.go
@@ -29,7 +29,10 @@ func (_ *NewlineRewriter) Rewrite(left, right []byte) ([]byte, error) {
 
 // implementation of Rewriter for ReadRewriter
 func (_ *ReadRewriter) Rewrite(left, right []byte) ([]byte, error) {
-	if line, err := stdin.ReadString('\n'); err != nil {
+	if line, err := stdin.ReadString('\n'); err == nil {
+		if len(line) > 0 && line[len(line)-1] == '\n' {
+			line = line[:len(line)-1]
+		}
 		return append(append(left, []byte(line)...), right...), nil
 	} else {
 		return []byte{}, err
